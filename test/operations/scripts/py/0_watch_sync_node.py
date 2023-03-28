684ce8fe7a2eef3484385a980cbc87d45059c53f
rink1969 <rink1969@gmail.com>
2023-03-27 22:04:11 -0400

    catch exception when check sync

--- test/operations/scripts/py/0_watch_sync_node.py
+++ test/operations/scripts/py/0_watch_sync_node.py
@@ -13,22 +13,27 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import pprint
 import threading
 import time
 import sys
 
 sys.path.append("test/utils")
 import util
-
+from logger import logger
 
 def watch_bn():
     bn = util.get_node_block_number("default")
     result = util.get_node_block_number("node4")
     while result <= bn:
         time.sleep(10)
-        result = util.get_node_block_number("node4")
-    pprint.pprint("sync node block-number: {result} > {bn}".format(result=result, bn=bn), indent=4)
+        logger.debug("syncing node block-number: {result} ... {bn}".format(result=result, bn=bn))
+        try:
+            result = util.get_node_block_number("node4")
+        except Exception as e:
+            logger.debug("Error occurred while getting node block number: {e}".format(e=e))
+            pass
+
+    logger.info("synced node block-number: {result} > {bn}".format(result=result, bn=bn))
     exit(0)
 
 
@@ -38,6 +43,6 @@ if __name__ == '__main__':
     t.start()
     t.join(timeout=600)
     if t.is_alive():
-        pprint.pprint("sync timeout")
+        logger.info("sync timeout")
         exit(1)
     exit(0)
